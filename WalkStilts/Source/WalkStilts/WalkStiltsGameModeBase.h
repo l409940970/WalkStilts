d8178be47ec4ee91b55799c25de7ca019ce6e705
LYM <409940970@qq.com>
2024-03-28 11:00:09 +0800

    1

---
+++ WalkStilts/Source/WalkStilts/WalkStiltsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WalkStiltsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WALKSTILTS_API AWalkStiltsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
